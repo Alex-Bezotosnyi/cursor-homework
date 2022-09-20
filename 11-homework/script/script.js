3cfa1db7dd5d9eac7f581e45551d0a42cb338232
Alex-Bezotosnyi <alexbezaua@gmail.com>
2022-09-20 11:03:32 +0300

    init: add "click"

--- 11-homework/script/script.js
+++ 11-homework/script/script.js
@@ -1,5 +1,5 @@
-document.addEventListener("keydown", function (event) {
-    switch (event.code) {
+document.addEventListener("keydown", function (ev) {
+    switch (ev.code) {
         case "KeyC":
             document.getElementById("keyC").play();
             break;
@@ -18,4 +18,44 @@ document.addEventListener("keydown", function (event) {
         default:
             alert("Press any key of: C, U, R, S or O")
     }
-})
+});
+
+class Melody {
+    constructor(el) {
+        this._el = el;
+        el.onclick = this.onClick.bind(this);
+    }
+
+    keyC() {
+        document.getElementById("keyC").play();
+    }
+
+    keyU() {
+        document.getElementById("keyU").play();
+    }
+
+    keyR() {
+        document.getElementById("keyR").play();
+    }
+
+    keyS() {
+        document.getElementById("keyS").play();
+    }
+
+    keyO() {
+        document.getElementById("keyO").play();
+    }
+
+    keyR2() {
+        document.getElementById("keyR").play();
+    }
+
+    onClick(event) {
+        let action = event.target.dataset.action;
+        if (action) {
+            this[action]();
+        }
+    }
+}
+
+new Melody(melody);
