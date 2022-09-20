9df7f17c4e762351ae0d14459ddccb6eccefa968
Alex-Bezotosnyi <alexbezaua@gmail.com>
2022-09-20 22:56:34 +0300

    init: renew dismiss-recover

--- 08-homework/script/08-homework.js
+++ 08-homework/script/08-homework.js
@@ -27,7 +27,7 @@ class Student {
     getAverageMark() {
         let dismiss = this.dismiss();
         let recover = this.recover();
-        return dismiss === false && recover === true
+        return dismiss === false && recover === false
             ? null : this.marks.reduce((a, b) => a + b) / this.marks.length;
     }
 
@@ -37,7 +37,7 @@ class Student {
     }
 
     recover() {
-        let marks = false;
+        let marks = true;
         return marks;
     }
 }
@@ -53,7 +53,7 @@ class BudgetStudent extends Student {
 ////// ідеї закінчилися щодо getInterval
 // https://developer.mozilla.org/ru/docs/Web/API/setInterval#%D0%BF%D1%80%D0%BE%D0%B1%D0%BB%D0%B5%D0%BC%D0%B0_%D1%81_this
     getScholarship() {
-        let message = "Ви отримали +1400 грн. стипендії";
+        let message = "Ви отримали 1400 грн. стипендії";
         // return window.setInterval(`getScholarship`, 1000)
         return this.getAverageMark() >= 4.0 ? message : "Ви НЕ отримали 1400 грн. стипендії";
     }
