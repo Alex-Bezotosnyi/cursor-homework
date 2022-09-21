e52f6a9877a15e5a4858621511635c78bafea3ff
Alex-Bezotosnyi <alexbezaua@gmail.com>
2022-09-21 09:43:09 +0300

    init: add div's using JS

--- 10-homework/script/script.js
+++ 10-homework/script/script.js
@@ -1,3 +1,14 @@
+let wrapperDiv = document.createElement("div");
+wrapperDiv.className = "wrapper";
+document.getElementsByTagName("body")[0].appendChild(wrapperDiv)
+
+for (let i = 0; i < 25; i++) {
+    let divBoxes = document.createElement("div");
+    divBoxes.className = "box";
+    divBoxes.innerText = i + 1;
+    wrapperDiv.appendChild(divBoxes);
+}
+
 function generateBlocks() {
     const oneRGB = () => Math.floor(Math.random() * 256);
     const fullRGB = () => "rgb(" + oneRGB() + "," + oneRGB() + "," + oneRGB() + ")";
